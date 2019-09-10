bca90cf0323aa7b8dd18962bae27ff21d9c6d1a0
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-09-10 13:26:10 +0200

    removed newrelic from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,3 @@
-// tslint:disable-next-line:no-var-requires
-require('newrelic');
-
 import { Module } from '@nestjs/common';
 import { HeroModule } from './hero/hero.module';
 import { VillainModule } from './villain/villain.module';
