e8098a782b3d9779384ca734b3f724d5bbdf5eee
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-09-10 11:57:25 +0200

    added New Relic for monitoring

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// tslint:disable-next-line:no-var-requires
+require('newrelic');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
